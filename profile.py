d9d3996d115bb5c83c0a55f9287d5622488e9dc6
qi0523 <1426042225@qq.com>
2022-12-08 18:01:33 +0800

    disk-image

--- profile.py
+++ profile.py
@@ -6,7 +6,7 @@ import geni.portal as portal
 # Import the ProtoGENI library.
 import geni.rspec.pg as rspec
 
-IMAGE = 'urn:publicid:IDN+clemson.cloudlab.us+image+containernetwork-PG0:owv1'
+IMAGE = 'urn:publicid:IDN+clemson.cloudlab.us+image+containernetwork-PG0:clem-openwhisk'
 
 pc = portal.Context()
 
